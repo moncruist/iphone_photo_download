5c51aabf366de5ea38479fce5a9afd0f4b7ff5b2
Konstantin Zhukov <moncruist@gmail.com>
2020-02-27 21:14:43 +0100

    Refactor options parser

--- main.cpp
+++ main.cpp
@@ -13,34 +13,90 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#include <iomanip>
-#include <iostream>
-
 #include "app.h"
 
 #include <boost/program_options.hpp>
+#include <iomanip>
+#include <iostream>
 
 namespace po = boost::program_options;
 
-int main(int argc, char *argv[]) {
+enum class Command { LIST_DEVICES, LIST_FILES };
+
+struct Options {
+    Command command;
+    int device_index;
+};
+
+namespace {
+
+const std::pair<const char*, Command> SUPPORTED_COMMANDS[] = {{"list", Command::LIST_DEVICES},
+                                                              {"list-files", Command::LIST_FILES}};
+
+}
+
+std::optional<Options> parse_options(int argc, char* argv[]) {
     po::options_description desc("All options");
-    desc.add_options()("command", po::value<std::string>()->required(), "Command")("help", "produce help message");
+
+    // clang-format off
+    desc.add_options()
+            ("command", po::value<std::string>()->required(), "Command")
+            ("help", "produce help message")
+            ("device", po::value<int>()->default_value(0), "Device number");
+    // clang-format on
 
     po::positional_options_description positional;
     positional.add("command", 1);
 
     po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(positional).run(), vm);
-    po::notify(vm);
+    po::parsed_options parsed = po::command_line_parser(argc, argv).options(desc).positional(positional).run();
+    po::store(parsed, vm);
+
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
+        return std::nullopt;
+    }
+
+    if (vm.count("command") == 0) {
+        std::cerr << "Missing command" << std::endl;
+        return std::nullopt;
+    }
+
+    const auto& command = vm["command"].as<std::string>();
+
+    auto pos = std::find_if(std::begin(SUPPORTED_COMMANDS), std::end(SUPPORTED_COMMANDS), [&](const auto& elem) {
+        return elem.first == command;
+    });
+    if (pos == std::end(SUPPORTED_COMMANDS)) {
+        std::cerr << "Unknown command: " << command << std::endl;
+        return std::nullopt;
+    }
+
+    po::notify(vm);
+
+    Options options {pos->second, vm["device"].as<int>()};
+    return options;
+}
+
+
+int main(int argc, char* argv[]) {
+    std::optional<Options> options = parse_options(argc, argv);
+    if (!options) {
         return 0;
     }
 
     App app;
 
-    app.print_device_list();
+    switch (options->command) {
+        case Command::LIST_DEVICES:
+            app.print_device_list();
+            break;
+
+        case Command::LIST_FILES:
+            app.open_camera(options->device_index);
+            break;
+    }
 
     return 0;
 }
